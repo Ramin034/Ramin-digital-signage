1f1f47ef57b762b2ae6879dfccda632f846dff24
Ramin034 <Ramin.Akbari03@gmail.com>
2026-04-23 12:15:05 -0600

    Added new weather symbols that have color

--- app.js
+++ app.js
@@ -62,18 +62,18 @@ function startClock() {
 }
 
 function getWeatherSymbol(code) {
-    if (code === 0)                return '☀ Clear';
-    if (code === 1)                return '🌤 Mostly Clear';
-    if (code === 2)                return '⛅ Partly Cloudy';
-    if (code === 3)                return '☁ Overcast';
-    if (code >= 45 && code <= 48) return '🌫 Foggy';
-    if (code >= 51 && code <= 55) return '🌦 Drizzle';
-    if (code >= 61 && code <= 65) return '🌧 Rainy';
-    if (code >= 71 && code <= 77) return '❄ Snowy';
-    if (code >= 80 && code <= 82) return '🌧 Showers';
-    if (code >= 85 && code <= 86) return '🌨 Snow Showers';
-    if (code >= 95 && code <= 99) return '⛈ Thunderstorm';
-    return '🌡 Unknown';
+    if (code === 0)                return '☀️ Clear';
+    if (code === 1)                return '🌤️ Mostly Clear';
+    if (code === 2)                return '⛅️ Partly Cloudy';
+    if (code === 3)                return '☁️ Overcast';
+    if (code >= 45 && code <= 48) return '🌫️ Foggy';
+    if (code >= 51 && code <= 55) return '🌦️ Drizzle';
+    if (code >= 61 && code <= 65) return '🌧️ Rainy';
+    if (code >= 71 && code <= 77) return '❄️ Snowy';
+    if (code >= 80 && code <= 82) return '🌧️ Showers';
+    if (code >= 85 && code <= 86) return '🌨️ Snow Showers';
+    if (code >= 95 && code <= 99) return '⛈️ Thunderstorm';
+    return '🌡️ Unknown';
 }
 
 async function loadWeather(url, title = 'Weather') {
